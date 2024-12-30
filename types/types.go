365ae8f3351358eb266c50b69708a5416db73ffb
Jack Woodgate <67034721+lampy255@users.noreply.github.com>
2024-12-30 13:51:43 +1300

    Update types.go

--- types/types.go
+++ types/types.go
@@ -4,32 +4,32 @@ type Env struct {
 	PUBLIC_HOST    string   // Public host for web interface
 	ADMIN_EMAIL    string   // Admin email
 	ADMIN_PASS     string   // Admin password
-	PRIVATE_KEY    string   // Private key for wireguard
-	PUBLIC_KEY     string   // Public key for wireguard
+	WG_PRIVATE_KEY string   // Private key for wireguard
+	WG_PUBLIC_KEY  string   // Public key for wireguard
+	DB_AES_KEY     string   // Base64 encoded 32 Byte AES key for encrypting private keys
 	SERVER_CIDR    string   // CIDR Network for tunnel addresses (optional)
 	NAME_SERVERS   []string // List of public DNS servers to use (optional)
 	INTERFACE_NAME string   // Override kernel interface name (optional)
 	WG_PORT        string   // Port for wireguard to listen on (optional)
-	UI_PORT        string   // Port for web interface to listen on (optional)
 	API_PORT       string   // Port for API to listen on (optional)
 }
 
 type Peer struct {
-	UUID             string   `json:"uuid"`
-	Hostname         string   `json:"hostname"`
-	Enabled          bool     `json:"enabled"`
-	PeerType         string   `json:"peerType"` // "wg-client", "tbm-client"
-	UpdatedMillis    int64    `json:"updatedMillis"`
-	PrivateKey       string   `json:"privateKey"`
-	PublicKey        string   `json:"publicKey"`
-	PreSharedKey     string   `json:"preSharedKey"`
-	KeepAliveMillis  int      `json:"keepAliveMillis"`
-	LocalTunAddress  string   `json:"localTunAddress"`  // The IP address of the client's tunnel interface
-	RemoteTunAddress string   `json:"remoteTunAddress"` // The IP address of the server's tunnel interface (future use)
-	RemoteSubnets    []string `json:"remoteSubnets"`    // A list of CIDR subnets that the peer can provide access to
-	AllowedSubnets   []string `json:"allowedSubnets"`   // A list of CIDR subnets that the peer is allowed to access
-	LastSeenMillis   int64    `json:"lastSeenMillis"`
-	LastIPAddress    string   `json:"lastIPAddress"`
+	UUID               string   `json:"uuid"`
+	Hostname           string   `json:"hostname"`
+	Enabled            bool     `json:"enabled"`
+	PeerType           string   `json:"peerType"` // "wg-client", "tbm-client"
+	UpdatedUnixMillis  int64    `json:"updatedUnixMillis"`
+	PrivateKey         string   `json:"privateKey"`       // Wireguard private key (stored encrypted with AES256)
+	PublicKey          string   `json:"publicKey"`        // Wireguard public key
+	PreSharedKey       string   `json:"preSharedKey"`     // Wireguard pre-shared key (stored encrypted with AES256)
+	KeepAliveMillis    int      `json:"keepAliveMillis"`  // Wireguard keep-alive interval in milliseconds
+	LocalTunAddress    string   `json:"localTunAddress"`  // The IP address of the client's tunnel interface
+	RemoteTunAddress   string   `json:"remoteTunAddress"` // The IP address of the server's tunnel interface (future use)
+	RemoteSubnets      []string `json:"remoteSubnets"`    // A list of CIDR subnets that the peer can provide access to
+	AllowedSubnets     []string `json:"allowedSubnets"`   // A list of CIDR subnets that the peer is allowed to access
+	LastSeenUnixMillis int64    `json:"lastSeenUnixMillis"`
+	LastIPAddress      string   `json:"lastIPAddress"`
 }
 
 type PeerExtended struct {
@@ -42,5 +42,17 @@ type UserAccount struct {
 	Email          string `json:"email"`
 	Role           string `json:"role"` // "user", "admin"
 	FailedAttempts int    `json:"failedAttempts"`
-	Suspended      bool   `json:"suspended"`
+}
+
+type LoginBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type APIKey struct {
+	UUID              string `json:"uuid"`
+	Hash              string `json:"hash"`
+	ExpiresUnixMillis int64  `json:"expiresUnixMillis"`
+	Role              string `json:"role"` // Future use
+	Name              string `json:"name"`
 }
