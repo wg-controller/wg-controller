bc6726df89d165a1f3e5f681a67228dbe6c9c5d2
Jack Woodgate <67034721+lampy255@users.noreply.github.com>
2025-01-07 11:30:23 +1300

    bugfix
    - Wait 2 minutes before displaying a client as offline

--- wg-controller-ui/src/utils/utils.ts
+++ wg-controller-ui/src/utils/utils.ts
@@ -1,59 +1,59 @@
-export function timeSinceString(unixMillis: number) {  
-    if (!unixMillis) {
-      return "never";
-    }
-
-    if (unixMillis < 0) {
-      return "never"; // Handle future dates
-    }
-
-    const unixSeconds = unixMillis / 1000;
-  
-    const now = Math.floor(Date.now() / 1000); // Get current time in seconds
-    const seconds = now - unixSeconds; // Calculate the difference
-  
-    if (seconds < 0) {
-      return "just now"; // Handle future dates
-    }
-  
-    const days = Math.floor(seconds / 86400);
-    const hours = Math.floor((seconds % 86400) / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-  
-    if (days > 0) {
-      return days + " days ago";
-    } else if (hours > 0) {
-      return hours + " hours ago";
-    } else if (minutes > 0) {
-      return minutes + " minutes ago";
-    } else {
-      return "just now";
-    }
+export function timeSinceString(unixMillis: number) {
+  if (!unixMillis) {
+    return "never";
   }
 
-  export function timeSinceSeconds(unixMillis: number) {
-    const thenSeconds = unixMillis / 1000;
-    const now = Math.floor(Date.now() / 1000); // Get current time in seconds
-    const seconds = now - thenSeconds; // Calculate the difference
-
-    return seconds
+  if (unixMillis < 0) {
+    return "never"; // Handle future dates
   }
 
-  export function BytesString(bytes: number) {
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-    if (bytes == 0) return "0 Byte";
-    const i = Math.floor(Math.log(bytes) / Math.log(1024));
-    return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
+  const unixSeconds = unixMillis / 1000;
+
+  const now = Math.floor(Date.now() / 1000); // Get current time in seconds
+  const seconds = now - unixSeconds; // Calculate the difference
+
+  if (seconds < 0) {
+    return "just now"; // Handle future dates
   }
 
-  export function NewUUID() {
-    return crypto.randomUUID();
-  }
+  const days = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
 
-  export function ArrayToCommas(arr: string[]) {
-    return arr.join(", ");
+  if (days > 0) {
+    return days + " days ago";
+  } else if (hours > 0) {
+    return hours + " hours ago";
+  } else if (minutes > 2) {
+    return minutes + " minutes ago";
+  } else {
+    return "just now";
   }
+}
 
-  export function CommasToArray(str: string) {
-    return str.split(", ");
-  }
+export function timeSinceSeconds(unixMillis: number) {
+  const thenSeconds = unixMillis / 1000;
+  const now = Math.floor(Date.now() / 1000); // Get current time in seconds
+  const seconds = now - thenSeconds; // Calculate the difference
+
+  return seconds;
+}
+
+export function BytesString(bytes: number) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes == 0) return "0 Byte";
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
+}
+
+export function NewUUID() {
+  return crypto.randomUUID();
+}
+
+export function ArrayToCommas(arr: string[]) {
+  return arr.join(", ");
+}
+
+export function CommasToArray(str: string) {
+  return str.split(", ");
+}
