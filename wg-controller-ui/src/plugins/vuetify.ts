d0246138f1fe845050ec1e2894a658f78420adc6
Jack Woodgate <67034721+lampy255@users.noreply.github.com>
2025-01-21 17:57:53 +1300

    Update vuetify.ts

--- wg-controller-ui/src/plugins/vuetify.ts
+++ wg-controller-ui/src/plugins/vuetify.ts
@@ -20,7 +20,7 @@ export default createVuetify({
         dark: false,
         colors: {
           primary: "#6390C7",
-          secondary: "rgb(0, 33, 72)",
+          secondary: "#002148",
           background: "rgb(210, 216, 222)",
           oddRow: "rgb(226, 229, 231)",
           surface: "rgb(235, 235, 235)",
